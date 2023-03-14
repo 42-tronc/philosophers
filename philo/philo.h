38b3470749ecd5babb8371dd01aecf7dbaba0261
Maplepy <croy@student.42lyon.fr>
2023-03-14 14:03:27 +0100

    add libs and changed unsigned to long in struct

--- philo/philo.h
+++ philo/philo.h
@@ -6,23 +6,26 @@
 /*   By: croy <croy@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 18:40:42 by croy              #+#    #+#             */
-/*   Updated: 2023/03/14 12:37:58 by croy             ###   ########lyon.fr   */
+/*   Updated: 2023/03/14 12:50:22 by croy             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
-#include <stddef.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
 
 typedef struct s_data
 {
-	unsigned	nb_philo;
-	unsigned	death_time;
-	unsigned	eat_time;
-	unsigned	sleep_time;
-	unsigned	eat_count;
+	long	nb_philo;
+	long	death_time;
+	long	eat_time;
+	long	sleep_time;
+	long	eat_count;
 }	t_data;
 
+int	ft_atoi(const char *str);
 
 #endif
