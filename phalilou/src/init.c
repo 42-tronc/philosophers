14deb4c607a620bf8023f072d6bd822fd87ea490
maple <github.register@slmail.me>
2023-07-31 14:23:30 +0200

    🔄 update: creates mutexes in a sep fn, cleanup

--- phalilou/src/init.c
+++ phalilou/src/init.c
@@ -6,17 +6,19 @@
 /*   By: croy <croy@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 16:56:14 by croy              #+#    #+#             */
-/*   Updated: 2023/07/31 11:02:58 by croy             ###   ########lyon.fr   */
+/*   Updated: 2023/07/31 14:09:50 by croy             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "philo.h"
+// ignore the variable set but not used warning
+#pragma GCC diagnostic ignored "-Wunused-but-set-variable"
+#include "../header/philo.h"
 
-static int	init_data(t_data *data, char **av)
+int	init_data(t_data *data, char **av)
 {
 	data->all_alive = 1;
 	data->nb_philo = ft_atoi(av[1]);
-	data->need_to_eat = data->nb_philo;
+	data->still_hungry = data->nb_philo;
 	data->time_to_die = ft_atoi(av[2]);
 	data->time_to_eat = ft_atoi(av[3]);
 	data->time_to_sleep = ft_atoi(av[4]);
@@ -39,52 +41,24 @@ static int	init_data(t_data *data, char **av)
 	return (0);
 }
 
-/* int	init_philo(t_data *data)
-{
-	int				i;
-	pthread_mutex_t	*fork_mutex;
-
-	data->philos = malloc(sizeof(t_philo) * data->nb_philo);
-	if (!data->philos)
-		return (NULL);
-	fork_mutex = malloc(sizeof(pthread_mutex_t) * data->nb_philo);
-	if (!fork_mutex)
-		return (NULL);
-	i = 0;
-	while (i < data->nb_philo)
-		pthread_mutex_init(&fork_mutex[i++], NULL);
-	data->fork_mutexes = fork_mutex;
-	pthread_mutex_init(&data->print, NULL);
-	i = 0;
-	while (i < data->nb_philo)
-	{
-		philos[i].id = i + 1;
-		philos[i].eaten = 0;
-		gettimeofday(&philos[i].last_meal, NULL);
-		philos[i].data = data; // Pass a pointer to the t_data structure
-		i++;
-	}
-	return (0);
-} */
-
 void	destroy_mutexes(t_data *data, int i)
 {
-	while (i >= 0)
-		pthread_mutex_destroy(&data->fork_mutexes[i--]);
 	pthread_mutex_destroy(&data->print_mutex);
 	pthread_mutex_destroy(&data->alive_mutex);
+	while (i >= 0)
+		pthread_mutex_destroy(&data->fork_mutexes[i--]);
 	// free(data->fork_mutexes);
-
 }
 
 int	init_mutexes(t_data *data)
 {
 	int	i;
 
-	if (pthread_mutex_init(&data->print, NULL))
+	if (pthread_mutex_init(&data->print_mutex, NULL))
 		return (EXIT_FAILURE);
 	if (pthread_mutex_init(&data->alive_mutex, NULL))
-		return (pthread_mutex_destroy(&data->print), EXIT_FAILURE);
+		return (pthread_mutex_destroy(&data->print_mutex), EXIT_FAILURE);
+	data->fork_mutexes = malloc(sizeof(pthread_mutex_t) * data->nb_philo);
 	i = 0;
 	while (i < data->nb_philo)
 	{
@@ -97,13 +71,18 @@ int	init_mutexes(t_data *data)
 
 int	init_philo(t_data *data)
 {
-	int				i;
+	int	i;
 
 	data->philos = malloc(sizeof(t_philo) * data->nb_philo);
 	if (!data->philos)
-		return (1);
+		return (EXIT_FAILURE);
+	data->forks = malloc(sizeof(int) * data->nb_philo);
+	if (!data->forks)
+		return (free(data->philos), EXIT_FAILURE);
 
-	init_mutexes(data);
+	// Create the mutexes
+	if (init_mutexes(data))
+		return (free(data->philos), free(data->forks), EXIT_FAILURE);
 	i = 0;
 	while (i < data->nb_philo)
 	{
