cca523db833c0cfdd9967407b62315d87aa9dd56
maple <github.register@slmail.me>
2023-07-31 14:21:54 +0200

    🎉 add: free.c

---
+++ phalilou/src/free.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: croy <croy@student.42lyon.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/31 11:34:22 by croy              #+#    #+#             */
+/*   Updated: 2023/07/31 14:11:12 by croy             ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../header/philo.h"
+
+void	free_data(t_data *data)
+{
+	free(data->philos);
+	free(data->forks);
+}
+
+void	free_mutexes(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (i < data->nb_philo)
+	{
+		pthread_mutex_destroy(&data->fork_mutexes[i]);
+		i++;
+	}
+	free(data->fork_mutexes);
+	pthread_mutex_destroy(&data->print_mutex);
+	pthread_mutex_destroy(&data->alive_mutex);
+}
