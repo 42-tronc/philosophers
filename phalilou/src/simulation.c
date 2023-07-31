3b005bf585650aaaeb105fc10f48e1ebd690df62
maple <github.register@slmail.me>
2023-07-31 11:03:56 +0200

    🎉 add: simulation.c with a pthread join to end the simulation

---
+++ phalilou/src/simulation.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   simulation.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maplepy <maplepy@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/31 09:54:48 by maplepy           #+#    #+#             */
+/*   Updated: 2023/07/31 10:38:25 by maplepy          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../header/philo.h"
+
+void	end_simulation(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (i < data->nb_philo)
+	{
+		pthread_join(data->philos[i].thread, NULL);
+		i++;
+	}
+}
