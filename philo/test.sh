3f233f0897b7e194c3c3aa42e0245d108478d2f3
maple <github.register@slmail.me>
2023-08-13 17:41:46 +0200

    🔄 update: add more tests

--- philo/test.sh
+++ philo/test.sh
@@ -36,13 +36,16 @@ build_project
 # declare -a test_args=("3 610 200 200 5")
 # declare -a test_args=("20 1100 500 500 20")
 # declare -a test_args=("5 300 200 200 2")
-declare -a test_args=("10 500 200 200 10" "5 300 100 100 5" "20 1100 500 500 20" "3 900 300 300 10" "5 300 200 200 2")
+# declare -a test_args=("10 500 200 200 10" "5 300 100 100 5" "20 1100 500 500 20" "3 900 300 300 10" "5 300 200 200 2")
+declare -a test_args=("5 800 200 200 10" "3 1000 299 300 20" "3 1000 300 299 20" "3 1000 330 330 20" "199 620 200 200 10" "200 420 200 200 10" "4 410 200 200 20" "20 200 99 100" "20 200 100 99 10" "1 410 200 200 10")
 
 for args in "${test_args[@]}"; do
     echo -e "\n\n${cyan}Testing with arguments: ${bold}$args$reset"
 
     # Run Valgrind helgrind test
     run_helgrind_test $args
+
+    ./philo $args
 
     # Run Valgrind memcheck test with specific options
     run_memcheck_test $args
